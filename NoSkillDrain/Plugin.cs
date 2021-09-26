1989a2a1e045fb755227dc685f94ec70e0a5e824
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2021-09-26 17:42:19 +0200

    initial

---
+++ NoSkillDrain/Plugin.cs
@@ -0,0 +1,81 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace NoSkillDrain
+{
+    [BepInPlugin("FixItFelix.no.skill.drain", Plugin.ModName, Plugin.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string Version = "1.0.0";
+        public const string ModName = "NoSkillDrain";
+        Harmony _Harmony;
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+#if DEBUG
+			Log = Logger;
+#else
+            Log = new ManualLogSource(null);
+#endif
+            _Harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        private void OnDestroy()
+        {
+            if (_Harmony != null) _Harmony.UnpatchSelf();
+        }
+    }
+
+    public static class NoSkillDrain
+    {
+        public static ConfigEntry<float> skillDrainMultiplier;
+
+        public static float applyModifierValue(float targetValue, float value)
+        {
+            if (value <= -100)
+                value = -100;
+            return targetValue + ((targetValue / 100) * value);
+        }
+
+        [HarmonyPatch(typeof(Skills), nameof(Skills.OnDeath))]
+        public static class NoSkillDrainTranspiler
+        {
+            private static MethodInfo method_Skills_LowerAllSkills = AccessTools.Method(typeof(Skills), nameof(Skills.LowerAllSkills));
+            private static MethodInfo method_LowerAllSkills = AccessTools.Method(typeof(NoSkillDrainTranspiler), nameof(NoSkillDrainTranspiler.LowerAllSkills));
+
+            /// <summary>
+            /// We replace the call to Skills.LowerAllSkills with our own stub, which then applies the death multiplier.
+            /// </summary>
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> il = instructions.ToList();
+
+                for (int i = 0; i < il.Count; ++i)
+                {
+                    if (il[i].Calls(method_Skills_LowerAllSkills))
+                    {
+                        il[i].operand = method_LowerAllSkills;
+                    }
+                }
+
+                return il.AsEnumerable();
+            }
+
+            public static void LowerAllSkills(Skills instance, float factor)
+            {
+                if (NoSkillDrain.skillDrainMultiplier.Value > -100.0f)
+                {
+                    instance.LowerAllSkills(NoSkillDrain.applyModifierValue(factor, NoSkillDrain.skillDrainMultiplier.Value));
+                }
+            }
+        }
+
+    }
+}
---
+++ NoSkillDrain/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using NoSkillDrain;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoSkillDrain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FixItFelix")]
+[assembly: AssemblyProduct("NoSkillDrain")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e04a89bc-d232-454b-84b9-a5a282bf6f6d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Plugin.Version)]
+[assembly: AssemblyFileVersion(Plugin.Version)]
